bd95bbebfdaccf889e2ed704d883e006ec4bb2d2
Austin Cesear <austincesear123@gmail.com>
2022-04-01 16:56:23 -0700

    added key to drumRows render

--- src/DrumRows/DrumRows.js
+++ src/DrumRows/DrumRows.js
@@ -1,32 +1,33 @@
 import "./DrumRows.css";
 import Square from "../Square/Square";
+import React from "react";
 
 const drumMap = ["Kick", "Snare", "HiHat", "Pluck"];
 
 const DrumRows = ({ activeColumn, pattern, setPattern }) => {
   return (
-    <>
-      <div className="drums-container">
-        {pattern.map((row, y) => {
-          return (
-            <>
-              <div className="drum-title" style={{gridRow: y+1}}>{drumMap[y]}</div>
-              <div className="drum-squares" style={{gridRow: y+1}}>
-                {row.map((value, x) => (
-                  <Square
-                    key={x}
-                    active={activeColumn === x}
-                    value={value}
-                    onClick={() => setPattern({ x, y, value })}
-                  />
-                ))}
-              </div>
-              <div className="three"></div>
-            </>
-          );
-        })}
-      </div>
-    </>
+    <div className="drums-container">
+      {pattern.map((row, y) => {
+        return (
+          <React.Fragment key={y}>
+            <div className="drum-title" style={{ gridRow: y + 1 }}>
+              {drumMap[y]}
+            </div>
+            <div className="drum-squares" style={{ gridRow: y + 1 }}>
+              {row.map((value, x) => (
+                <Square
+                  key={x}
+                  active={activeColumn === x}
+                  value={value}
+                  onClick={() => setPattern({ x, y, value })}
+                />
+              ))}
+            </div>
+            <div className="three"></div>
+          </React.Fragment>
+        );
+      })}
+    </div>
   );
 };
 
